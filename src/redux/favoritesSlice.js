5a99478b5711e2d29b6bb0a40d3fb24d6b738e46
Shohaib hossain <khanshohaibhossain@gmail.com>
2025-11-16 23:09:43 +0600

    implement the toggle favorite

--- src/redux/favoritesSlice.js
+++ src/redux/favoritesSlice.js
@@ -8,7 +8,20 @@ const favoritesSlice = createSlice({
   name: "favorites",
   initialState,
   reducers: {
-   
+    toggleFavorite: (state, action) => {
+      const recipe = action.payload;
+      const index = state.favoriterecipes.findIndex(
+        (favrecipe) => favrecipe.idFood === recipe.idFood
+      );
+
+      if (index !== -1) {
+        // recipe exists, so remove it
+        state.favoriterecipes.splice(index, 1);
+      } else {
+        // recipe doesn't exist, so add it
+        state.favoriterecipes.push(recipe);
+      }
+    },
   },
 });
 
